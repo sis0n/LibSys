47f7f586df034a495506e61a9f19f53ae7e6e268
Joshua Colmo <joshuapaulmedina@gmail.com>
2025-09-07 22:32:51 +0800

    Setup: favicon & phosphor icons(Updated dev)

--- public/index.php
+++ public/index.php
@@ -1,5 +1,21 @@
-<?php
-require __DIR__ . '/../vendor/autoload.php';
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Library Online Software</title>
+    <!-- Tailwind.v4 -->
+    <link href="/LibSys/public/css/output.css" rel="stylesheet">
+    <!-- PHOSPHOR ICONS -->
+    <link rel="stylesheet" type="text/css"
+        href="https://cdn.jsdelivr.net/npm/@phosphor-icons/web@2.1.2/src/regular/style.css" />
+</head>
+
+<body>
+
+    <?php
+    require __DIR__ . '/../vendor/autoload.php';
 
 use App\Config\Router;
 
@@ -18,3 +34,6 @@ $url = $_GET['url'] ?? 'login';
 $method = $_SERVER['REQUEST_METHOD'];
 
 $router->resolve($url, $method);
+?>
+
+</html>

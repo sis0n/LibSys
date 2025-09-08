1b0921b9a2a7beed012e055bbb31c62d0572ba5f
Joshua Colmo <joshuapaulmedina@gmail.com>
2025-09-08 16:00:51 +0800

    feat: initial footer (finalized)

--- src/Controllers/dashboardController.php
+++ src/Controllers/dashboardController.php
@@ -1,28 +1,33 @@
 <?php
 namespace App\Controllers;
 
-class DashboardController{
+class DashboardController
+{
     public function admin()
     {
-        include __DIR__ . '/../Views/layout/header.php';
+        include __DIR__ . '/../Views/partials/header.php';
         include __DIR__ . '/../Views/dashboard/admin.php';
+        include __DIR__ . '/../Views/partials/footer.php';
     }
-    
+
     public function librarian()
     {
         include __DIR__ . '/../Views/partials/header.php';
         include __DIR__ . '/../Views/dashboard/librarian.php';
+        include __DIR__ . '/../Views/partials/footer.php';
     }
 
     public function student()
     {
         include __DIR__ . '/../Views/partials/header.php';
         include __DIR__ . '/../Views/dashboard/student.php';
+        include __DIR__ . '/../Views/partials/footer.php';
     }
 
     public function superadmin()
     {
         include __DIR__ . '/../Views/partials/header.php';
         include __DIR__ . '/../Views/dashboard/superadmin.php';
+        include __DIR__ . '/../Views/partials/footer.php';
     }
 }

72147311be140ce8343e074cba0fa6271a0e63b2
Joshua Colmo <joshuapaulmedina@gmail.com>
2025-09-08 15:48:05 +0800

    feat: header (with routing)

--- src/Controllers/dashboardController.php
+++ src/Controllers/dashboardController.php
@@ -4,21 +4,25 @@ namespace App\Controllers;
 class DashboardController{
     public function admin()
     {
+        include __DIR__ . '/../Views/layout/header.php';
         include __DIR__ . '/../Views/dashboard/admin.php';
     }
 
     public function librarian()
     {
+        include __DIR__ . '/../Views/partials/header.php';
         include __DIR__ . '/../Views/dashboard/librarian.php';
     }
 
     public function student()
     {
+        include __DIR__ . '/../Views/partials/header.php';
         include __DIR__ . '/../Views/dashboard/student.php';
     }
 
     public function superadmin()
     {
+        include __DIR__ . '/../Views/partials/header.php';
         include __DIR__ . '/../Views/dashboard/superadmin.php';
     }
 }

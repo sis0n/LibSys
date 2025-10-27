6ea8cebf0fd418ca8a3efbeb2260fa4afc08cce7
adrianoalwyn <adrianoalwyn>
2025-10-27 23:14:38 +0800

    bug: faculty dashboard path

--- src/Controllers/authController.php
+++ src/Controllers/authController.php
@@ -39,6 +39,12 @@ class AuthController extends Controller
                 case 'student':
                     header("Location: /libsys/public/student/dashboard");
                     exit;
+                case 'faculty':
+                    header("Location: /libsys/public/faculty/dashboard");
+                    exit;
+                case 'staff':
+                    header("Location: /libsys/public/staff/dashboard");
+                    exit;
                 case 'scanner':
                     header("Location: /libsys/public/scanner/attendance");
                     exit;
@@ -106,9 +112,9 @@ class AuthController extends Controller
                 $redirect = '/libsys/public/librarian/dashboard';
             } elseif (User::isStudent($user)) {
                 $redirect = '/libsys/public/student/dashboard';
-            } elseif (User::isScanner($user)) {
+            } elseif (User::isFaculty($user)) {
                 $redirect = '/libsys/public/faculty/dashboard';
-            } elseif (User::isScanner($user)) {
+            } elseif (User::isStaff($user)) {
                 $redirect = '/libsys/public/staff/dashboard';
             } elseif (User::isScanner($user)) {
                 $redirect = '/libsys/public/scanner/attendance';
